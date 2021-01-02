545df1f58ad8b7e2bce2a9dfac9ffdd3ad94a6e4
jval1972 <jvalavanis@gmail.com>
2021-01-02 14:15:14 +0200

    Updated year in copyright notice

--- ter_quantize.pas
+++ ter_quantize.pas
@@ -1,7 +1,7 @@
 //------------------------------------------------------------------------------
 //
 //  DD_TERRAIN: Terrain Generator
-//  Copyright (C) 2020 by Jim Valavanis
+//  Copyright (C) 2020-2021 by Jim Valavanis
 //
 //  This program is free software; you can redistribute it and/or
 //  modify it under the terms of the GNU General Public License
